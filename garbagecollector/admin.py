ed2e912c07775304d05d2d5cc6d1fb6bd3d0f85a
Ilya Stepanov <istepanov@renderedsource.com>
2012-07-29 13:06:18 +0700

    Admin page improvements

--- garbagecollector/admin.py
+++ garbagecollector/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from garbagecollector.models import User
+from garbagecollector.models import User, MacAddress
 
-admin.site.register(User)
+class MacAddressInline(admin.StackedInline):
+    model = MacAddress
+    extra = 1
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [MacAddressInline]
+
+admin.site.register(User, UserAdmin)
