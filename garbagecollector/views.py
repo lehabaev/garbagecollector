e214df8d23064bc966434ef12b15e562cd44b8f9
Ilya Stepanov <istepanov@renderedsource.com>
2012-07-30 12:36:06 +0700

    Fix query duplicating

--- garbagecollector/views.py
+++ garbagecollector/views.py
@@ -10,7 +10,7 @@ def index(request):
 
 def get_online(request):
     mac_addresses = network().get_online_mac_addesses()
-    online_users = User.objects.filter(macaddress__address__in = mac_addresses)
+    online_users = User.objects.filter(macaddress__address__in = mac_addresses).distinct()
     online_ids = online_users.values_list('id', flat=True)
     offline_users = User.objects.exclude(id__in = online_ids)
 
